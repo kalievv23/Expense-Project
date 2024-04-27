a760ee961dcebf0dacd53875cb4b8f814ea73038
kalievv23 <kaliev2004kg@gmail.com>
2024-04-27 19:20:01 +0600

    added hello text

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Expense from './components/Expense';
 function App() {
   return (
     <div className="App">
+      <h1>Hello World</h1>
       <Expense/>
     </div>
   );
@@ -14,8 +15,18 @@ export default App;
 
 
 
-
+// Проектти биринчи жолу жуктоп жаткандагы командалар: 
 
 // git init 
 // git add .
+// git commit -m "first commit"
+// git remote add origin https://github.com/kalievv23/Expense-Project.git
+// git push -u origin master
 
+
+
+// Проектти озгортуп жаткандагы командалар: 
+
+// git add .
+// git commit -m "first commit"
+// git push -u origin master
